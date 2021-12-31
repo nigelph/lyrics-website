f318740026b723c176315c08b956bac64e16ce1a
Nigel Phan <nigel.phan11@gmail.com>
2021-12-31 19:24:43 +1300

    created basic navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import navStyle from './navbar.module.css'
 
 export default function Navbar() {
     return (
-        <div>
-            this is the navbar
+        <div id={navStyle['nav-container']}>
+
+            <ul className={navStyle['nav-list']}>
+                <a href="#">Home</a>
+                <a href="#">One</a>
+                <a href="#">Two</a>
+                <a href="#">Three</a>
+                <a href="#">Four</a>
+            </ul>
         </div>
     )
 }
