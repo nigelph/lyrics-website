ec421a28c7b481321816485c6cef59718910b2f4
Nigel Phan <nigel.phan11@gmail.com>
2022-01-01 19:44:32 +1300

    created basic footer structure

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import footerStyle from './footer.module.css'
+
+export default function Footer() {
+    return (
+        <footer className={footerStyle['footer-container']}>
+            <div className={footerStyle['footer-content-container']}>
+                <div>
+                    <h3>Contact Us</h3>
+
+                    <a href="https://google.com">
+                        <p>one</p>
+                    </a>
+                    <a href="#">
+                        <p>one</p>
+                    </a>
+                </div>
+                <div>
+                    <h3>More Information</h3>
+                    <a href="#">
+                        <p>Shipping Information</p>
+                    </a>  <a href="#">
+                        <p>Return and Refund Policy</p>
+                    </a>
+                </div>
+                <div>
+                    <h3>About Us</h3>
+                    <a href="#">
+                        <p>one</p>
+                    </a>  <a href="#">
+                        <p>one</p>
+                    </a>
+                </div>
+            </div>
+
+
+
+        </footer>
+    )
+}
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
+import Footer from '../../components/Footer/Footer'
+
 
 export default function Home() {
     return (
         <div>
             <Navbar />
             this is the home page
+            <Footer />
         </div>
     )
 }
