e0a75ecfffff7655121ec1f7c27018395c4742d2
Nigel Phan <nigel.phan11@gmail.com>
2022-01-08 21:15:24 +1300

    fixed footer positioning

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,10 +2,11 @@ import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import Footer from '../../components/Footer/Footer'
 import SongForm from '../../components/SongForm/SongForm'
+import homeStyle from '../Home/home.module.css'
 
 export default function Home() {
     return (
-        <div>
+        <div className={homeStyle['home-container']}>
             <Navbar />
             <SongForm />
             <div id='show-lyrics'></div>
