fb8199ab40271da6dce819494cf49a498d57c5a1
Nigel Phan <nigel.phan11@gmail.com>
2022-01-08 21:02:55 +1300

    added styling to form components and formatted song lyrics on page

--- src/components/SongForm/SongForm.js
+++ src/components/SongForm/SongForm.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import formStyle from './SongForm.module.css'
+import InputBox from './components/InputBox/InputBox'
+import SubmitButton from './components/SubmitButton/SubmitButton'
 
 function showLyrics(e) {
     e.preventDefault()
@@ -19,23 +22,25 @@ function showLyrics(e) {
         .then(res => res.json())
         .then((data) => {
             let lyricArea = document.getElementById('show-lyrics')
-
+            let test = JSON.stringify(data)
+            let something = test.replace(new RegExp('\r?\n', 'g'), '<br />');
             lyricArea.innerHTML = ''
             console.log(data.lyrics)
-            lyricArea.innerHTML += data.lyrics;
+            lyricArea.innerHTML += data.lyrics.replace(new RegExp('\n', 'g'), "<br>");
         })
 }
 
 
 export default function SongForm() {
     return (
+        <div className={formStyle['form-container']}>
+            <form onSubmit={showLyrics} className={formStyle['form-sytle']}>
+                <InputBox placeholder='Enter Artist' id='artistName' />
+                <InputBox placeholder='Enter Song Name' id='songName' />
+                <SubmitButton id='submit' />
 
-        <form onSubmit={showLyrics}>
-            <input type="text" placeholder='Enter Artist' id="artistName"></input>
-            <input type="text" placeHolder='Enter Song Name' id="songName"></input>
-
-            <input type="submit" id="submit"></input>
-        </form>
+            </form>
+        </div>
 
     )
 }
